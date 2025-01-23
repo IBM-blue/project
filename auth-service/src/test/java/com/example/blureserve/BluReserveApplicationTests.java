b6ccb10ec7a4f659650edc8f73ec121725b024a1
shreyash-01 <singhshreyash0075@gmail.com>
2025-01-23 15:54:33 +0530

    Created Project

---
+++ auth-service/src/test/java/com/example/blureserve/BluReserveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blureserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BluReserveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
